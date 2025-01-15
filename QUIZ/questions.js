351a3d60a48a84cf4800dccdbbc289a6464c0338
Felipe <128333262+fegomss@users.noreply.github.com>
2025-01-14 22:25:59 -0300

    Add files via upload

---
+++ QUIZ/questions.js
@@ -0,0 +1,93 @@
+export default [
+    {
+      question: "Quanto é 1 + 2?",
+      answers: [
+        { option: "3", correct: true  },
+        { option: "2", correct: false },
+        { option: "4", correct: false },
+        { option: "12", correct: false },
+      ],
+    },
+    {
+      question: "Quanto é 5 x 2?",
+      answers: [
+        { option: "9", correct:  false },
+        { option: "10", correct: true  },
+        { option: "11", correct: false },
+        { option: "12", correct: false },
+      ],
+    },
+    {
+      question: "Quanto é 10 x 8?",
+      answers: [
+        { option: "800", correct: false },
+        { option: "90", correct:  false },
+        { option: "80", correct:  true  },
+        { option: "108", correct: false },
+      ],
+    },
+    {
+      question: "Quanto é 7 x 7?",
+      answers: [
+        { option: "70", correct: false },
+        { option: "56", correct: false },
+        { option: "49", correct: true  },
+        { option: "77", correct: false },
+      ],
+    },
+    {
+      question: "Em Que Ano Eu(Felipe) Nasci?",
+      answers: [
+        { option: "2008", correct: false },
+        { option: "2007", correct: true  },
+        { option: "2006", correct: false },
+        { option: "2005", correct: false },
+      ],
+    },
+    {
+      question: "Em Que Dia/Mês Eu(Felipe) Nasci?",
+      answers: [
+        { option: "10/05", correct: false },
+        { option: "31/09", correct: false },
+        { option: "23/10", correct: true  },
+        { option: "26/11", correct: false },
+      ],
+    },
+    {
+      question: "Quem Nasceu No Mesmo Dia Que Eu(Felipe)",
+      answers: [
+        { option: "Pelé", correct:  true  },
+        { option: "Claudia Arraia", correct: false },
+        { option: "Roberto Carlos", correct: false },
+        { option: "Luciano Hulk", correct: false },
+      ],
+    },
+    {
+      question: "Qual é o planeta mais próximo do Sol?",
+      answers: [
+        { option: "Marte", correct: false },
+        { option: "Vênus", correct: false },
+        { option: "Terra", correct: false },
+        { option: "Mercúrio", correct: true },
+      ],
+    },
+    {
+      question: "Quem escreveu 'Dom Quixote'?",
+      answers: [
+        { option: "Dra. Becker", correct: false},
+        { option: "William Shakespeare", correct: false },
+        { option: "Machado de Assis", correct: false },
+        { option: "Miguel de Cervantes", correct: true },
+      ],
+    },
+    {
+      question: "Em que ano o homem pisou na Lua pela primeira vez?",
+      answers: [
+        { option: "2026", correct: false},
+        { option: "1969", correct: true },
+        { option: "1968", correct: false },
+        { option: "1971", correct: false },
+      ],
+    },
+    
+  ];
